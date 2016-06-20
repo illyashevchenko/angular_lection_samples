f9a1c404e684bef875395cf8dcb5343ec54f7043
Illia Shevchenko <illyashevchenko@gmail.com>
2016-06-20 20:21:06 +0300

    Add interceptors

--- 16.http.js
+++ 16.http.js
@@ -4,26 +4,55 @@ class ListCtrl {
       .then(classes => {
         this.classes = classes;
       });
+
+    Classes.getByName();
   }
 }
 
 angular.module('simpleApp', ['ui.router'])
   .controller('ListCtrl', ListCtrl)
-  .factory('Classes', $http => ({
+  .factory('Classes', ($http, $log) => ({
     query() {
       return $http.get('16.http.json')
         .then(classes =>
           classes.data);
     },
 
-
     getById(id) {
       return this.query()
         .then(classes =>
           classes.find(item => item.id === id));
+    },
+
+    getByName() {
+      return $http.get('16.http-names.json')
+        .then(names => {
+          $log.debug('Received names: ', names.data);
+          return names;
+        });
     }
   }))
+
   .config($httpProvider => {
     $httpProvider.useLegacyPromiseExtensions(false)
       .useApplyAsync(true);
+  })
+
+  .factory('LogErrorInterceptor', ($q, $log) => ({
+    responseError(rejection) {
+      $log.error('Something went wrong with request to: ', rejection.config.url);
+
+     return $q.reject(rejection);
+    }
+  }))
+
+  .factory('MyInterceptor', () => ({
+    request() {},
+    requestError() {},
+    response() {},
+    responseError() {}
+  }))
+  .config($httpProvider => {
+    // $httpProvider.interceptors.push('MyInterceptor');
+    $httpProvider.interceptors.push('LogErrorInterceptor');
   });
